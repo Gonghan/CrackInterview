75c4d51f31d4febf16ee19b7451fcdcdcc3630bd
gonghan <wanggonghan@gmail.com>
2013-10-13 03:21:15 -0700

    Hanoi

--- src/com/gonghan/chapter3/Q4.java
+++ src/com/gonghan/chapter3/Q4.java
@@ -14,31 +14,95 @@ package com.gonghan.chapter3;
 public class Q4 {
 
 	private int NumofDisks;
-
+	private HanoiStack first;
+	private HanoiStack second;
+	private HanoiStack third;
+	
 	public Q4(int n) {
 		this.NumofDisks = n;
+		this.first = new HanoiStack(n,"first");
+		this.second = new HanoiStack(n,"second");
+		this.third = new HanoiStack(n,"third");
+		for (int i = n; i > 0; i--) {
+			this.first.push(i);
+		}
+	}
+	
+	public void start(){
+		this.move(first, second, third,this.NumofDisks);
 	}
 
 	// recursive
-	public void move(HanoiStack start, HanoiStack mid, HanoiStack dest) {
+	public void move(HanoiStack start, HanoiStack mid, HanoiStack dest,int rob) {
+		// Ending condition, start has no nodes,
+		// n=number of robs in start
+		// Step 1. Move (n-1)robs in start to mid.
+		// Step 2. Move the last rob in start to dest
 
+		if (rob==0) {
+			// Ending condition, start has no nodes
+			return;
+		}
+		
+		//Step 1.
+		this.move(start, dest, mid,rob-1);
+		int nth=start.pop();
+		dest.push(nth);
+		String message=String.format("Move %d from Stack %s to Stack %s", nth,start.name,dest.name);
+		System.out.println(message);
+		this.move(mid, start, dest, rob-1);
 	}
 
 	// display three stacks, just for tests
 	public void display() {
+		System.out.println("Show first stack");
+		while(!first.isEmpty()){
+			System.out.print(first.pop()+", ");
+		}
+		System.out.println("Show second stack");
+		while(!second.isEmpty()){
+			System.out.print(second.pop()+", ");
+		}
+		System.out.println("Show third stack");
+		while(!third.isEmpty()){
+			System.out.print(third.pop()+", ");
+		}
 
 	}
 }
 
 class HanoiStack {
 	private int index;
-	
-	
-	public void push(int value){
-		
+	private int array[];
+	public String name;
+
+	public HanoiStack(int size,String name) {
+		this.array = new int[size];
+		this.index = 0;
+		this.name=name;
 	}
-	
-	public int pop(){
-		return 0;
+
+	public void push(int value) {
+		this.array[index] = value;
+		index++;
+	}
+
+	public int pop() {
+		if (index <= 0)
+			return Integer.MAX_VALUE;
+		index--;
+		return this.array[index];
+	}
+
+	public boolean isEmpty() {
+		if (this.index <= 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean checkValid() {
+		return false;
 	}
 }
--- src/com/gonghan/chapter3/Q4_test.java
+++ src/com/gonghan/chapter3/Q4_test.java
@@ -1,7 +1,15 @@
 package com.gonghan.chapter3;
 
+import org.junit.Test;
+
 
 public class Q4_test {
 
+	@Test
+	public void test(){
+		Q4 q4=new Q4(3);
+		q4.start();
+		q4.display();
+	}
 }
 
